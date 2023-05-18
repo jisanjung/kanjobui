99f983cb0605deda9a0fbbe0c44f76f372264c8e
Jason Jung <jasonjung@Jasons-MacBook-Pro.local>
2023-05-18 16:55:26 -0400

    configure: setup google sso

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Login = () => {
+
+  const handleLoginSuccess = response => {
+    console.log(`jwt token: ${response.credential}`);
+  }
+
+  useEffect(() => {
+    const { VITE_GOOGLE_CLIENT_ID, VITE_GOOGLE_CLIENT_SECRET } = import.meta.env;
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: VITE_GOOGLE_CLIENT_ID,
+      callback: handleLoginSuccess
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+
   return (
     <h1>
         Login
+        <div id='signInDiv'></div>
     </h1>
   )
 }
