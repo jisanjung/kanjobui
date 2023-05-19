763f612f86aa77ecaeb9ae4eca4d3737d5a4c673
Jisan Jung <jjjung13@gmail.com>
2023-05-19 15:02:27 -0400

    chore: wrap fetching user data in try catch

--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -1,12 +1,17 @@
 const { VITE_KANJOB_API_ENDPOINT } = import.meta.env;
 
 const doesUserExist = async email => {
-    const response = await fetch(`${VITE_KANJOB_API_ENDPOINT}/users/${email}`);
-    const data = await response.json();
-    if (data === null) {
+    try {
+        const response = await fetch(`${VITE_KANJOB_API_ENDPOINT}/users/${email}`);
+        const data = await response.json();
+        if (data === null) {
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.log(error);
         return false;
     }
-    return true;
 }
 
 export { doesUserExist }
