c0d742d6342e1ebdf5c8ae7ca238e4b444b9eaa8
Jisan Jung <jjjung13@gmail.com>
2023-05-25 23:24:18 -0400

    configure: install and initialize react dnd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 const Home = () => {
+
+  const [column, setColumn] = useState({
+    "applied": {
+      title: "Applied",
+      items: []
+    },
+    "interview": {
+      title: "Interview",
+      items: []
+    },
+    "in_progress": {
+      title: "In Progress",
+      items: []
+    },
+    "offer": {
+      title: "Offer",
+      items: []
+    }
+  });
+
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>Home</h1>
+      <DragDropContext onDragEnd={e => console.log(e)}>
+        
+      </DragDropContext>
+    </div>
   )
 }
 
